68aa8931908aee9211793b76d07c72120b1699d2
Avanish <avanishkumar4400@gmail.com>
2025-10-12 23:30:14 +0530

    add and update

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,8 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "http://localhost:5174", 
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "https://hospital-managment-1-t0qi.onrender.com",
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
